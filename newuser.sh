f2c9767c58207482d04f9a85fca2f33ce4746940
TC-matthew-wenger <57915406+TC-matthew-wenger@users.noreply.github.com>
2019-12-10 10:09:28 -0800

    Add files via upload

---
+++ newuser.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+fullname=$(/usr/bin/osascript -e 'Tell application "System Events" to display dialog "Please enter the users First & Last name or select Cancel." default answer "First Last"' -e 'text returned of result' 2>/dev/null)
+
+username=$(/usr/bin/osascript -e 'Tell application "System Events" to display dialog "Please enter the username or select Cancel." default answer "first.last"' -e 'text returned of result' 2>/dev/null)
+
+#Create user account
+sudo /usr/sbin/sysadminctl -addUser $username -fullName "$fullname" -password userpass -admin
+sudo /System/Library/CoreServices/ManagedClient.app/Contents/Resources/createmobileaccount -n $username
+echo 'success'
