0d1aa2bdee1223294d26609fac87dd4b65d26bcb
TC-matthew-wenger <57915406+TC-matthew-wenger@users.noreply.github.com>
2019-12-10 10:53:57 -0800

    Update newuser.sh
    firevault enable

--- newuser.sh
+++ newuser.sh
@@ -4,6 +4,10 @@ fullname=$(/usr/bin/osascript -e 'Tell application "System Events" to display di
 username=$(/usr/bin/osascript -e 'Tell application "System Events" to display dialog "Please enter the username or select Cancel." default answer "first.last"' -e 'text returned of result' 2>/dev/null)
 
 #Create user account
-sudo /usr/sbin/sysadminctl -addUser $username -fullName "$fullname" -password userpass -admin
+sudo /usr/sbin/sysadminctl -addUser $username -fullName "$fullname" -password - -admin
 sudo /System/Library/CoreServices/ManagedClient.app/Contents/Resources/createmobileaccount -n $username
+# Enable Firevault
+sudo fdesetup enable
+# Transfer secureToken
+sudo sysadminctl interactive -secureTokenOn $username -password -
 echo 'success'
